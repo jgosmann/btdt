7d0dcb00c347ea6484845256cfbe3d8ea6e45f05
jgosmann <jgosmann@users.noreply.github.com>
2026-03-10 19:06:59 +0000

    deploy: c95209d7880fb5ca7f84e6780651cf342ee11953

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-236dd065.js',
+                path_to_root + 'searchindex-55a29348.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
